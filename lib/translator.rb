5861025771e31ac9076469b8e9441a318e81cfae
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-12-01 18:42:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ def load_library(file_path)
  
  emoticons.each do |key, value| 
    result_hash["get_emoticon"][value[0]] = emoticons
-   result_hash
+   result_hash["get_meaning"]
 end
 
 def get_japanese_emoticon
