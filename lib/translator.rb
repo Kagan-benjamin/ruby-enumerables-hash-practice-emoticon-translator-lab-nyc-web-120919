e7781a61f5cd90905a4bd7090ac0b97f17d75ef6
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-12-01 18:33:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,13 +3,13 @@ emoticons = YAML.load_file('emoticons.yml')
 
 
 def load_library
-  # code goes here
+ 
 end
 
 def get_japanese_emoticon
-  # code goes here
+ 
 end
 
 def get_english_meaning
-  # code goes here
+  
 end
