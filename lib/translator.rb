d6e656cb9369fce0e0ee4aba7f2ab47dea9fb3e5
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-12-01 18:49:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   translation = load_library(file_path)["get emoticon"][emoticon]
-  translation ? translation :  
+  translation ? translation : "Sorry, that emoticon doesn't exist." 
 end
 
 def get_english_meaning
